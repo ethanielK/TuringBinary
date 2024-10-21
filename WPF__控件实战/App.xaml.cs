b0767d326a051bb6081e1c536a4de59e9056add1
robot <robot@major.com>
2024-10-21 15:01:19 +0800

    Add WPF__控件实战

---
+++ WPF__控件实战/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF__控件实战
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
