1249b855132dd72327813f8a34dccd42f215c714
robot <robot@major.com>
2024-10-30 15:06:35 +0800

    Update WPF 资源、样式、控件模板、触发器

--- WPF__样式资源控件模板/MainWindow.xaml.cs
+++ WPF__样式资源控件模板/MainWindow.xaml.cs
@@ -20,5 +20,29 @@ namespace WPF__样式资源控件模板
         {
             InitializeComponent();
         }
+
+        private void btnResource_Click(object sender, RoutedEventArgs e)
+        {
+            Resource resource = new Resource();
+            resource.Show();
+        }
+
+        private void btnFontResource_Click(object sender, RoutedEventArgs e)
+        {
+            FontResource resource = new FontResource();
+            resource.Show();
+        }
+
+        private void btnStyle_Click(object sender, RoutedEventArgs e)
+        {
+            Style style = new Style();
+            style.Show();
+        }
+
+        private void btnControlTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            ControlTemplate controlTemplate = new ControlTemplate();
+            controlTemplate.Show();
+        }
     }
 }
---
+++ WPF__样式资源控件模板/Resource.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF__样式资源控件模板
+{
+    /// <summary>
+    /// Resource.xaml 的交互逻辑
+    /// </summary>
+    public partial class Resource : Window
+    {
+        public Resource()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeColor1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["PresentColor1"] = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#89c26e"));
+        }
+
+        private void btnChangeColor2_Click(object sender, RoutedEventArgs e)
+        {
+            LinearGradientBrush brush = new LinearGradientBrush()   // 对象初始化器
+            {
+                StartPoint = new Point(0, 0),
+                EndPoint = new Point(1, 1),
+                GradientStops = new GradientStopCollection
+                {
+                    new GradientStop{Offset =0,Color = (Color)ColorConverter.ConvertFromString("#f02fc2")},
+                    new GradientStop{Offset =1,Color = (Color)ColorConverter.ConvertFromString("#6094ea")}
+                }
+            };
+            this.Resources["PresentColor2"] = brush;
+        }
+    }
+}
