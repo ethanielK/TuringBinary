6c3279a052bc596e84230c24b841b57d69015d81
Ethaniel <lk1981640629@gmail.com>
2024-12-05 21:22:44 +0800

    🛖Add WPF Commad Part;🪵 Remove Empty Winform Solution.

--- WPF__命令/MainWindow.xaml.cs
+++ WPF__命令/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF__命令
 {
@@ -19,7 +11,58 @@ namespace WPF__命令
         public MainWindow()
         {
             InitializeComponent();
+            this.InitializeCommand();
         }
 
+        // 声明并定义命令
+        private RoutedCommand clearCmd = new RoutedCommand("Clear",typeof(MainWindow));
+
+        // 声明命令初始化的方法，包括命令赋值、命令关联等
+        private void InitializeCommand()
+        {
+            // 为命令额外添加快捷键触发方式
+            this.clearCmd.InputGestures.Add(new KeyGesture(Key.C, ModifierKeys.Alt));
+
+            // 为命令源指定命令和目标
+            this.btnCommand.Command = this.clearCmd;
+            this.btnCommand.CommandTarget = this.textBox;
+
+            // 创建命令关联
+            // ①创建实例
+            CommandBinding cb = new CommandBinding();
+            // ②指定命令
+            cb.Command = this.clearCmd;
+            // ③将CanExecute和Execute订阅相应的事件处理程序
+            cb.CanExecute += new CanExecuteRoutedEventHandler(cb_CanExecute);
+            cb.Executed += new ExecutedRoutedEventHandler(cb_Execute);
+
+            // 设置命令关联的位置
+            this.stackPanel.CommandBindings.Add(cb);
+        }
+
+        // 判断事件是否可执行的事件处理器
+        private void cb_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(this.textBox.Text))
+            {
+                e.CanExecute= false;
+            }
+            else
+            {
+                e.CanExecute= true;
+            }
+
+            //避免向上继续执行而降低性能
+            e.Handled= true;
+        }
+
+        // 命令到达目标后要执行的事件处理器
+        private void cb_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.textBox.Clear();
+
+            //避免向上继续执行而降低性能
+            e.Handled = true;
+        }
     }
 }
