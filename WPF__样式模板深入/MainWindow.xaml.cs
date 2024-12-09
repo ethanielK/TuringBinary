19fdf75d5c1358f0f2664d394f897126cb043dec
Ethaniel <lk1981640629@gmail.com>
2024-12-09 21:08:39 +0800

    ⭕Add WPF__样式模板深入 Part.

---
+++ WPF__样式模板深入/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace WPF__样式模板深入
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrigger_Click(object sender, RoutedEventArgs e)
+        {
+            TriggerWindow window = new TriggerWindow();
+            window.Show();
+        }
+
+        private void btnMultiTrigger_Click(object sender, RoutedEventArgs e)
+        {   
+            MultiTriggerWindow window = new MultiTriggerWindow();
+            window.Show();
+        }
+    }
+}
---
+++ WPF__样式模板深入/TriggerWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WPF__样式模板深入
+{
+    /// <summary>
+    /// SetterWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class TriggerWindow : Window
+    {
+        public TriggerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
