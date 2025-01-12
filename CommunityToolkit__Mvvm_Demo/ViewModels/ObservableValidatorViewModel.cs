0a506cd1ded72deb078b62468bca5163d6713e89
Ethaniel <lk1981640629@gmail.com>
2025-01-12 22:01:20 +0800

    Add ObservableVadilator part.

---
+++ CommunityToolkit__Mvvm_Demo/ViewModels/ObservableValidatorViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CommunityToolkit__Mvvm_Demo.ViewModels
+{
+    public partial class ObservableValidatorViewModel : ObservableValidator
+    {
+        [ObservableProperty]
+        [Range(1, 168)]
+        [Required]
+        private int _age;
+
+        partial void OnAgeChanged(int value)
+        {
+            ValidateProperty(value, nameof(Age));
+        }
+
+        [ObservableProperty]
+        [Required]
+        [EmailAddress]
+        private string _email;
+
+        [ObservableProperty]
+        private string? _errorMessage;
+
+        [RelayCommand]
+        void Submit()
+        {
+            ValidateAllProperties();
+
+            //_errorMessage = HasErrors ? string.Join(Environment.NewLine,GetErrors().Select(e=> e.ErrorMessage)) : "";
+
+            if (HasErrors)
+            {
+                _errorMessage = string.Join(Environment.NewLine, GetErrors().Select(e => e.ErrorMessage));
+            }
+            else
+            {
+                _errorMessage = "";
+            }
+        }
+    }
+}
