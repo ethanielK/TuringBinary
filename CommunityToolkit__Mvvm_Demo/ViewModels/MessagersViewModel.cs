406812b2b71c2dec3eaf72b5b1cf512fd69ea466
Ethaniel <lk1981640629@gmail.com>
2025-01-16 21:18:40 +0800

    Add 'Messagers' part.

---
+++ CommunityToolkit__Mvvm_Demo/ViewModels/MessagersViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace CommunityToolkit__Mvvm_Demo.ViewModels
+{
+    public partial class MessagersViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _name = "Default Name";
+
+        [RelayCommand]
+        public void Submit()
+        {
+            WeakReferenceMessenger.Default.Send<NameMessage,string>(new NameMessage("Name has changed."),"tokenA");
+        }
+
+        [RelayCommand]
+        public void SubmitWithTokenB()
+        {
+            WeakReferenceMessenger.Default.Send<NameMessage, string>(new NameMessage("Name has changed."), "tokenB");
+        }
+
+        public MessagersViewModel()
+        {
+            WeakReferenceMessenger.Default.Register<NameMessage,string>(this,"tokenB" ,(_, message) =>
+            {
+                Name = message.content;
+            });
+        }
+
+        public record NameMessage(string content);
+    }
+}
